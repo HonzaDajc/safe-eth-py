719995ada871c055e4b899c3ca3cc7a68e5cb627
jenikd <jd@fantom.foundation>
2024-11-30 12:18:36 +0100

    Add custom network

--- gnosis/safe/addresses.py
+++ gnosis/safe/addresses.py
@@ -845,11 +845,14 @@ MASTER_COPIES: Dict[EthereumNetwork, List[Tuple[str, int, str]]] = {
     EthereumNetwork.SONIC_FAKENET: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 10, "1.3.0"),
     ],
-    EthereumNetwork.SONIC_DEVNET: [
+    EthereumNetwork.SONIC_BLAZE: [
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 10, "1.3.0"),
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 10, "1.3.0+L2"),
     ],
-    
+    EthereumNetwork.SONIC_MAINNET: [
+        ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 10, "1.3.0"),
+        ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 10, "1.3.0+L2"),
+    ],    
     EthereumNetwork.ROOTSTOCK_MAINNET: [
         ("0x3E5c63644E683549055b9Be8653de26E0B4CD36E", 3891238, "1.3.0+L2"),
         ("0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552", 3891240, "1.3.0"),
@@ -2003,7 +2006,10 @@ PROXY_FACTORIES: Dict[EthereumNetwork, List[Tuple[str, int]]] = {
     EthereumNetwork.SONIC_FAKENET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 10),  # v1.3.0
     ],
-    EthereumNetwork.SONIC_DEVNET: [
+    EthereumNetwork.SONIC_BLAZE: [
+        ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 10),  # v1.3.0
+    ],
+    EthereumNetwork.SONIC_MAINNET: [
         ("0xa6B71E26C5e0845f74c812102Ca7114b6a896AB2", 10),  # v1.3.0
     ],
     EthereumNetwork.ROOTSTOCK_MAINNET: [
